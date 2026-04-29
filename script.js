08f554413c454390ff335567518f32ad5d01b692
terasynctech <terasynctech.gh@gmail.com>
2026-04-30 00:05:08 +0530

    Fix message formatting in orderNow function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function orderNow(item){
-  let msg = "🍱 RanchiHomeBites Order %0A%0AItem: " + item;
+  let msg = "🍱 RanchiHomeBites Order%0A%0AItem: " + item;
   window.open("https://wa.me/917633801161?text=" + msg);
 }
